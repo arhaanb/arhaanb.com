047a23380a641423f7be4b49b0569da8e2aac5a0
arhaanb <arhaanb@gmail.com>
2023-09-06 14:35:31 +0530

    upgrade deps

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,12 @@ import path from 'path'
 import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-import { minifyHtml } from 'vite-plugin-html'
 
 export default defineConfig({
 	plugins: [
 		vue({
 			include: [/\.vue$/, /\.md$/]
 		}),
-		minifyHtml(),
 
 		Components({
 			resolvers: IconsResolver({
